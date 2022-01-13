e03b27add7f3bfd5d77505e1aee56398e9852430
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-01-13 17:59:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 alert('starting');
 
 let canvas = document.getElementById('canvas');
-alert(canvas);
-//let ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d');
 
-//canvas.width = window.innerWidth;
-//canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 function loop() {
-  alert('I am cool');
+  requestAnimationFrame(() => {
+    alert('I am cool');           
+  });
 }
 
 loop();
