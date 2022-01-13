988710d934ae2d6e11e53d0067f59dec6e293bb1
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-01-13 17:04:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,14 +6,8 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function gameLoop() {
-  alert('sas');
-  /*ctx.clearRect(0, 0, canvas.width, canvas.height);
+function loop() {
+  alert('I am cool');
+}
 
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
-  requestAnimationFrame(gameLoop);*/
-};
-
-gameLoop();
+loop();
