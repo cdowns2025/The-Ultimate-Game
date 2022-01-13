57057f1393f9afa9ebae7c2725269500063babd5
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-01-13 16:51:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const canvas = document.getElementById('game_canvas');
+const ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function gameLoop() {
+  requestAnimationFrame(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  });
+};
+
+gameLoop();
