450986d3cfe0ab3c759dd3e03c95d05669c41605
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-01-13 17:05:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 alert('starting');
 
-const canvas = document.getElementById('game_canvas');
+/*const canvas = document.getElementById('game_canvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight;*/
 
 function loop() {
   alert('I am cool');
