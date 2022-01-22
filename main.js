c1d38f7b3e9638388fb71420f072c786d0158e48
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-01-22 13:16:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ alert('starting');
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 800;
+canvas.height = 720;
 
 function loop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
