90c022807e4cb705916ef21b7f6e349f94895a4b
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-01-22 13:20:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
   let ctx = canvas.getContext('2d');
 
   canvas.width = 800;
-  canvas.height = window.height;
+  canvas.height = window.innerHeight - 100;
 
   function animate() {
   }
