1e9113b7902484c5882c5e34674f821606605caa
portalh <93876124+portalh@users.noreply.github.com>
2022-02-05 15:50:39 -0500

    I added basic input system and a little bit of stage management for starting the game

--- game.js
+++ game.js
@@ -10,22 +10,50 @@ window.addEventListener('load', () => {
     GAME_OVER: false,
   };
 
+  const player = {
+    x: 300,
+    y: 300,
+    width: 30,
+    height: 30,
+    health: 3
+  }
+
   canvas.width = 720;
   canvas.height = window.innerHeight - 100;
+
+  //Inputs
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode == 13) { //starts the game when "+" is pressed
+      if (GAME_STATES.START == true) {
+        GAME_STATES.ACTIVE = true;
+        GAME_STATES.START = false;
+      }
+    }
+  });
 
   function animate() {
     // clear the screen
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    if (GAME_STATES.START == true) { //draws the start screen
+      ctx.fillStyle = 'black';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.font = '100px VT323';
-    ctx.fillStyle = 'white';
-    ctx.fillText('SHIP DEFENDERS', 100, canvas.height / 2);
-    
-    requestAnimationFrame(animate);
+      ctx.font = '100px VT323';
+      ctx.fillStyle = 'white';
+      ctx.fillText('SHIP DEFENDERS', 100, canvas.height / 2);
+      ctx.font = '50px VT323';
+      ctx.fillText("Press 'enter' to start", 180, (canvas.height / 2) + 70);
+    }
+    if (GAME_STATES.ACTIVE == true) { //draws the active state of the game
+      ctx.fillStyle = 'blue';
+      ctx.fillRect(player.x, player.y, player.width, player.height);
+    }
   }
 
-  animate();
+  function main() {
+    animate();
+    requestAnimationFrame(main);
+  }
+
+  main();
 });
