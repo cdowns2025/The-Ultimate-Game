cd33b11c8b4218ccb545472a91be41194e50b74d
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-12 11:33:18 -0500

    Update Ship.js

--- Ship.js
+++ Ship.js
@@ -1,7 +1,7 @@
 class Ship {
   constructor(config) {
     this.level = new Image();
-    //this.level.src = "grid.png";
+    this.level.src = "grid.png";
     //this.level.onload = () => {
       //this.levelLoaded = true;
     //};
