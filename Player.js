ba8c4fa8d442a0c76e28a990ead939384b3b8f46
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-12 12:18:48 -0500

    Update Player.js

--- Player.js
+++ Player.js
@@ -4,14 +4,30 @@ class Player {// the player class global attributes
         this.y = config.y || 0;
         this.width = config.width || 5;
         this.height = config.height || 5;
-        //this.dir = 1;
-        //this.vAxis = 0;
-        //this.hAxis = 0;
+        
+        
+        this.movingProgressRemaining = 16;
+        this.direction = config.direction || "down";
+        
+        this.directionUpdate = {
+            "down": ["y", 1],
+            "up": ["y", -1],
+            "left": ["x", -1],
+            "right": ["x", 1],
+        }
         
     }
     
-    update(direction) {
-        this.x++;
+    update(state) {
+        this.updatePosition();
+    } 
+    
+    updatePosition(state) {
+        if (this.movingProgressRemaining > 0) {
+            const [property, change] = this.directionUpdate[this.direction];
+            this[property] += change;
+            this.movingProgressRemaining -= 1;
+        }
     }
     
     draw(ctx) {
