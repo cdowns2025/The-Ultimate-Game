130b91c04a2417cd083358531d0909958a50abd9
haveneg <havenedward24@gmail.com>
2022-02-12 11:19:47 -0500

    player filtering

--- game.js
+++ game.js
@@ -1,5 +1,25 @@
 class Game {
   constructor(config) {
-    this.canvas = config.querySelector(".game-canvas");
+    this.canvas = config.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+    
+  }
+  
+  step() {
+    //this.player.draw();
+    //this.ship.draw(this.ctx);
+    
+    requestAnimation(() => {
+      step();
+    });
+  }
+  
+  init() {
+    //this.player = new Player(50, 50, 30, 30, 1);
+    
+    //his.ship = new Ship();
+    alert('hias');
+    
+    this.step();
   }
 }
