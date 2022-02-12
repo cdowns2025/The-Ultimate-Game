9520ed3af99207f98d24258b4cffbf2a4f41c264
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-12 11:46:46 -0500

    Update Game.js

--- Game.js
+++ Game.js
@@ -6,7 +6,8 @@ class Game {
   }
   
   step() {
-    this.ship.draw(this.ctx);
+    //this.ship.draw(this.ctx);
+    this.ctx.drawImage(this.image, 0, 0);
     this.player.draw(this.ctx);    
     
     requestAnimation(() => {
@@ -17,7 +18,12 @@ class Game {
   init() {
     this.player = new Player(50, 50, 30, 30, 1);
     
-    this.ship = new Ship();
+    //this.ship = new Ship();
+    this.image = new Image();
+    this.image.src = "grid.png";
+    this.image.onload = () => {
+      this.imageLoaded = true;
+    };
     
     this.step();
   }
