3e68a1f3ecf9eb39c99ad32f454e2def7e5a4222
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-12 10:48:29 -0500

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,5 +1,12 @@
 class Game {
   constructor(config) {
     this.canvas = config.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+    
+  }
+  
+  init() {
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
