1838118442aecc25e835d3eefe566fb1d2b9d5db
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-12 12:04:25 -0500

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,19 +1,19 @@
 class Player {// the player class global attributes
-    constructor(x, y, width, height, src) {
-        this.x = x;
-        this.y = y;
-        //this.cx = Math.floor(this.x / globalRatio);
-        //this.cy = Math.floor(this.y / globalRatio);
-        this.width = width;
-        this.height = height;
-        this.src = src;
-        this.dir = 1;
-        this.vAxis = 0;
-        this.hAxis = 0;
+    constructor(config) {
+        this.x = config.x || 0;
+        this.y = config.y || 0;
+        this.width = config.width || 5;
+        this.height = config.width || 5;
+        //this.dir = 1;
+        //this.vAxis = 0;
+        //this.hAxis = 0;
+        
     }
-    move() {
-
+    
+    update(direction) {
+        this.x++;
     }
+    
     draw(ctx) {
         ctx.fillStyle = 'blue';
         ctx.fillRect(this.x, this.y, this.width, this.height);
