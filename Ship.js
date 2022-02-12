ede056242571454d52c0db18ec8534fd573f392a
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-12 11:40:33 -0500

    Update Ship.js

--- Ship.js
+++ Ship.js
@@ -3,15 +3,14 @@ class Ship {
     this.image = new Image();
     this.image.src = "grid.png";
     this.image.onload = () => {
-      alert('loaded');
       this.levelLoaded = true;
-      alert(this.levelLoaded);
     };
     //alert('hias');
   }
   
   draw(ctx) {
     //alert('drawing');
+    alert(this.levelLoaded);
     if (this.levelLoaded) {
       alert('we are drawing')
       ctx.drawImage(this.image, 0, 0); 
