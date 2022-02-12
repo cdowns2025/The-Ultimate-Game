53f93ba59fd3ddebdc2111a5a4712faa101eeae9
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-12 11:14:00 -0500

    Update Game.js

--- Game.js
+++ Game.js
@@ -7,7 +7,7 @@ class Game {
   
   step() {
     //this.player.draw();
-    this.grid.draw(this.ctx);
+    this.ship.draw(this.ctx);
     
     requestAnimation(() => {
       step();
@@ -17,7 +17,7 @@ class Game {
   init() {
     //this.player = new Player(50, 50, 30, 30, 1);
     
-    this.grid = new Grid();
+    this.ship = new Ship();
     
     this.step();
   }
