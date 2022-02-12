e329c3e8df3828c05430c9f8857ef9740aeb0009
haveneg <havenedward24@gmail.com>
2022-02-12 17:27:22 -0500

    update

--- Ship.js
+++ Ship.js
@@ -6,20 +6,15 @@ class Ship {
     this.image.onload = () => {
       this.isLoaded = true;
     };
-    //alert('hias');
   }
   
   draw(ctx) {
-    //alert('drawing');
     alert(this.isLoaded);
     if (this.isLoaded) {
-      alert('we are drawing')
       ctx.drawImage(this.image, 0, 0); 
     }
-    
     return;
   }
-  
   init() {
     
   }
