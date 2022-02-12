670352bc6decd55b090beba5577f4c5f190916ef
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-12 11:10:58 -0500

    Update Ship.js

--- Ship.js
+++ Ship.js
@@ -1,6 +1,14 @@
 class Ship {
   constructor(config) {
-    
+    this.level = new Image();
+    this.level.src = "grid.png";
+    this.level.onload = () => {
+      this.levelLoaded = true;
+    };
+  }
+  
+  draw(ctx) {
+    ctx.drawImage(this.level, 0, 0);
   }
   
   init() {
