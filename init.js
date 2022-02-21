a22c4fc026827d4b0090e00ef2832c0f08092be5
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-21 16:51:09 -0500

    Update init.js

--- init.js
+++ init.js
@@ -1,8 +1,12 @@
+//Wait for the window to load
 window.addEventListener("load", () => {
-  
+ 
+  //Create the game!
   const game = new Game({
+    //Pass in the game container from the DOM
     element: document.querySelector(".game-container"),
   });
   
+  //Initialize the game
   game.init();
 });
