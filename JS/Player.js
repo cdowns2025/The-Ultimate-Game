06d94323ecee2913ffd9b401a9ed0a0c7676f666
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-21 17:30:28 -0500

    Update Player.js

--- JS/Player.js
+++ JS/Player.js
@@ -46,7 +46,10 @@ class Player {
     }
     
     draw(ctx) {
+        const x = this.x + utils.asGrid(12) - this.x;
+        const y = this.y + utils.asGrid(12) - this.y;
+        
         ctx.fillStyle = 'blue';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillRect(x, y, this.width, this.height);
     }
 }
