48f74746b7fccd53ad7a92c809f8f0db5e1f40a9
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-21 15:21:52 -0500

    Update Player.js

--- Player.js
+++ Player.js
@@ -38,6 +38,6 @@ class Player {// the player class global attributes
     
     draw(ctx) {
         ctx.fillStyle = 'blue';
-        ctx.fillRect(this.x + (this.movingProgressConstant / 2 - this.width), this.y + (this.movingProgressConstant / 2 - this.height), this.width, this.height);
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
