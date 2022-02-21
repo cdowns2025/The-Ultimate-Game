c0d636f9a29635aff527c4e0d901dd969ea983dc
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-21 17:32:15 -0500

    Update Player.js

--- JS/Player.js
+++ JS/Player.js
@@ -46,8 +46,8 @@ class Player {
     }
     
     draw(ctx) {
-        const x = this.x + utils.asGrid(6) - this.x;
-        const y = this.y + utils.asGrid(6) - this.y;
+        const x = this.x + utils.asGrid(6, this.width, this.game.gridSize) - this.x;
+        const y = this.y + utils.asGrid(6, this.height, this.game.gridSize) - this.y;
         
         ctx.fillStyle = 'blue';
         ctx.fillRect(x, y, this.width, this.height);
