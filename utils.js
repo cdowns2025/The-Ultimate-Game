2f800e461f206d3e4981e654a6192f3ed3dbe625
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-21 15:38:29 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const utils = {
   asGrid(coord, measurement, gridSize) {
-    console.log((coord * gridSize) + (gridSize / 2) - (measurement / 2))
+    console.log(coord)
     return (coord * gridSize) + (gridSize / 2) - (measurement / 2);
   }
 }
