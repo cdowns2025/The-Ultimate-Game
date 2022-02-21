31b96b9926d6c78858163d0aa80b9ef2681ff318
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-21 15:04:14 -0500

    Update Player.js

--- Player.js
+++ Player.js
@@ -6,9 +6,8 @@ class Player {// the player class global attributes
         this.height = config.height || 5;
         
         
-        this.movingProgressRemaining = 5;
+        this.movingProgressRemaining = 0;
         this.direction = config.direction || "right";
-        //this.playerDI = new DirectionInput(); //this is probably temporary, because it creates a direction input object in the player class. I'm only doing it right now because it seems a lot more complex to manage the input directions from Game.js
         
         this.directionUpdate = {
             "down": ["y", 1],
@@ -20,24 +19,18 @@ class Player {// the player class global attributes
     }
     
     update(state) {
-        alert("update");
-        //this.playerDI.init();//updates the inputs
-        this.updatePosition(state);//updates the player's position
+        if (this.movingProgressRemaining > 0) {
+            this.updatePosition(); //updates the player's position
+        } else {
+            this.direction = state.arrow;
+            this.movingProgressRemaining = 32
+        }
     } 
     
-    updatePosition(state) {
-        alert(state);
-        /*if (this.movingProgressRemaining == 0) {
-            if (this.playerDI.heldDirections.length > 0) { //checks if there's any direction currently being held
-                this.direction = this.playerDI.direction;//sets the player's direction to the current direction in the input
-                this.movingProgressRemaining = 5;
-            }
-        }
-        if (this.movingProgressRemaining > 0) {
-            const [ property, change ] = this.directionUpdate[this.direction];
-            this[property] += change;
-            this.movingProgressRemaining -= 1;
-        }*/
+    updatePosition(state) {        
+        const [ property, change ] = this.directionUpdate[this.direction];
+        this[property] += change;
+        this.movingProgressRemaining -= 1;
     }
     
     draw(ctx) {
