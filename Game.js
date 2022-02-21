c91cee68c77f1ceefc18f6f81fb3f1b7466a1894
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-21 15:08:49 -0500

    Update Game.js

--- Game.js
+++ Game.js
@@ -3,12 +3,28 @@ class Game {
     this.canvas = config.element.querySelector(".game-canvas");
     this.ctx = this.canvas.getContext("2d");
     
+    this.gridSize = 32;
+    this.gridColor = "black";
+    this.toggleGrid = true; // Draws a grid to the screen for testing purposes
+    
+  }
+  
+  drawGrid() {
+    for (let c = 0; c < Math.float(this.canvas.width / 32); c++) {
+      this.ctx.fillStyle = this.gridColor;
+      this.ctx.fillRect(c * 32, 0, 2, this.canvas.height);
+    }
   }
   
   startGameLoop() {
     const step = () => {
       //Clear the screen
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      
+      //If we want a grid
+      if (this.toggleGrid) {
+        this.drawGrid();
+      }
 
       this.player.update({
         arrow: this.directionInput.direction,
@@ -31,6 +47,7 @@ class Game {
       y: 10,
       width: 5,
       height: 5,
+      gridSize: this.gridSize,
     });
     
     this.directionInput = new DirectionInput();
