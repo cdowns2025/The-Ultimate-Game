87eb3e5c73dd4384c3749a1f09b4ce5d0f047cae
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-28 16:23:00 -0500

    Update Game.js

--- JS/Game.js
+++ JS/Game.js
@@ -19,87 +19,63 @@ step() {
 */
 
 class Game {
-  constructor(config) {
-    // Pulling the canvas from the DOM and getting the context of it
-    this.canvas = config.element.querySelector(".game-canvas");
-    this.ctx = this.canvas.getContext("2d");
-    
-    //Information to due with grid-based movement
-    this.gridSize = 8;
-    
-    //Specifications for the toggleable drawable grid
-    this.toggleGrid = false; 
-   
-    //Whenever we add cutscenes, this will help us know if one is going on
-    this.isCutscenePlaying = false;
-    
-  }
+    constructor(config) {
+        // Pulling the canvas from the DOM and getting the context of it
+        this.canvas = config.element.querySelector(".game-canvas");
+        this.ctx = this.canvas.getContext("2d");
 
-  startGameLoop() {
+        //Information to due with grid-based movement
+        this.gridSize = 8;
+
+        //Specifications for the toggleable drawable grid
+        this.toggleGrid = false; 
+
+        //Whenever we add cutscenes, this will help us know if one is going on
+        this.isCutscenePlaying = false;
+
+        this.map = null;
+
+    }
+
+    startGameLoop() {
     //Start the official "game loop"
     const step = () => {
-      //Clear the screen
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-     
-      /*this.ship.update(this.player);
-      this.ship.draw(this.ctx);*/
+        //Clear the screen
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-      //Update
-      this.player.update({
-        arrow: this.directionInput.direction,
-      });
-      this.player.sprite.draw(this.ctx, this.player);  
-     
-     
-      //If we want a grid
-      if (this.toggleGrid) {
-        this.grid.draw(this.ctx, this.player);
-      }
-     
-      //Call this function again at earliest convience / how fast your computer can run it
+        this.map.drawLowerLayer(this.ctx);
+
+        this.map.gameObjects.forEach(object => {
+            object.update({
+                arrow: this.directionInput.direction,
+            })
+        });
+
+        this.map.gameObjects.forEach(object => {
+            object.sprite.draw(this.ctx);
+        });
+
+        this.map.drawUpperLayer(this.ctx);
+
+        //Call this function again at earliest convience / how fast your computer can run it
         requestAnimationFrame(() => {
           step();
         });
     };
-    
+
     //Actually starting the loop
     step();
-  }
-  
-  init() {
-    //Creating a new player
-    /*this.player = new Player({
-      x: utils.asGrid(0, this.gridSize),
-      y: utils.asGrid(0, this.gridSize),
-      width: 7,
-      height: 7,
-      isPlayerControlled: true,
-      game: this,
-    });*/
-      
-    this.player = new Person({
-        isPlayerControlled: true,
-        x: utils.asGrid(0, this.gridSize),
-        y: utils.asGrid(0, this.gridSize),
-    });
-   
-   //Create the grid
-   this.grid = new Grid({
-      gridColor: "white",
-      gridLineWidth: 1,
-      gridSize: this.gridSize,
-      canvas: this.canvas,
-    });
-   
-   this.ship = new Ship({
-     canvas: this.canvas,
-   });
-    
-    //Setting up direction input for the player character
-    this.directionInput = new DirectionInput();
-    this.directionInput.init();
-    
-    //Start the game loop
-    this.startGameLoop();
-  }
+    }
+
+    init() {
+
+        this.map = new Map(window.Maps["Level1"]);
+
+        //Setting up direction input for the player character
+        this.directionInput = new DirectionInput();
+        this.directionInput.init();
+
+        //Start the game loop
+        this.startGameLoop();
+    }
 }
