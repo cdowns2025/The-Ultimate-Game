d76a3ca9d509d0c8360764ca80fe878a8917e922
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-28 16:51:31 -0500

    Update Map.js

--- JS/Map.js
+++ JS/Map.js
@@ -28,10 +28,12 @@ window.Maps = {
                 isPlayerControlled: true,
                 x: utils.asGrid(0),
                 y: utils.asGrid(0),
+                color: "red",
             }),
             enemy1: new Enemy({
                 x: utils.asGrid(5),
                 y: utils.asGrid(5),
+                color: "purple",
             })
         }
     }
