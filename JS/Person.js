8984f122984e272c286093fe1e35446c2f87e422
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-28 16:09:15 -0500

    Create Person.js

---
+++ JS/Person.js
@@ -0,0 +1,32 @@
+class Person extends GameObject {
+    constructor(config) {
+        super(config);
+        
+        this.movingProgressRemaining = 0;
+        
+        this.isPlayerControlled = config.isPlayerControlled || false;
+        
+        this.directionUpdate = {
+            "down": ["y", 1],
+            "up": ["y", -1],
+            "right": ["x", 1],
+            "left": ["x", -1],
+        }
+    }
+    
+    update(state) {
+        if (this.movingProgressRemaing > 0) {
+            this.updatePosition();
+        } else {
+            if (this.isPlayerControlled && state.arrow) {
+                this.direction = state.direction;
+            }
+        }
+    }
+    
+    updatePosition() {
+        const [property, change] = this.directionUpdate[this.direction];
+        this[property] += change;
+        this.movingProgressRemaining--;
+    }
+}
