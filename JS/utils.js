8dbb80be271da877a4b6ca06d40b16b914c0a224
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-02-28 16:50:30 -0500

    Update utils.js

--- JS/utils.js
+++ JS/utils.js
@@ -1,10 +1,10 @@
 const utils = {
   //A function that takes a grid coordinate, like 5 and transfers it into a pixel value, like 85.5
-  asGrid(n, gridSize) {
-    return n * gridSize;
+  asGrid(n) {
+    return n * 16;
   }, 
    //A function that takes a pixel coordinate, like 85.5 and transfers it into a grid value, like 5
-  asPixel(coord, gridSize) {
-    return Math.floor(coord / gridSize);
+  asPixel(coord) {
+    return Math.floor(coord / 16);
   }
 }
