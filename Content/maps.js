2b9bee3ff5b83dd9af9dd3803845102709579283
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-01 06:46:14 -0500

    Update maps.js

--- Content/maps.js
+++ Content/maps.js
@@ -3,17 +3,21 @@ window.Maps = {
         lowerSrc: "grid2.png",
         upperSrc: null,
         gameObjects: {
-            player: new Person({
-                isPlayerControlled: true,
-                x: utils.asGrid(0),
-                y: utils.asGrid(0),
-                color: "blue",
-            }),
-            enemy1: new Enemy({
-                x: utils.asGrid(5),
-                y: utils.asGrid(5),
-                color: "purple",
-            })
+            allies: {
+                player: new Person({
+                    isPlayerControlled: true,
+                    x: utils.asGrid(0),
+                    y: utils.asGrid(0),
+                    color: "blue",
+                }),
+            }, 
+            enemies: {
+                enemy1: new Enemy({
+                    x: utils.asGrid(5),
+                    y: utils.asGrid(5),
+                    color: "purple",
+                })
+            }
         }
     }
 }
