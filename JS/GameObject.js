f231b5e98eb562ffdfb9c0ed40f4074a0176d4c4
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-01 19:51:37 -0500

    Update GameObject.js

--- JS/GameObject.js
+++ JS/GameObject.js
@@ -1,5 +1,6 @@
 class GameObject {
     constructor(config) {
+        this.map = config.map;
         this.x = config.x || 0;
         this.y = config.y || 0;
         this.direction = config.direction || "down";
