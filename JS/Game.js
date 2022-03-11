14e8b224d39bb646cb409a0db268f8bfe33207a3
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-11 16:57:46 -0500

    Update Game.js

--- JS/Game.js
+++ JS/Game.js
@@ -39,6 +39,7 @@ class Game {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
          
             this.UI.update(this);
+            this.score++;
             
             const player = this.map.gameObjects.allies.player; 
 
