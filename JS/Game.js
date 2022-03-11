7f2f61c40547cecf19cf29a5279cdc65691e7151
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-11 18:20:59 -0500

    Update Game.js

--- JS/Game.js
+++ JS/Game.js
@@ -106,7 +106,7 @@ class Game {
     async init() {
      
         this.startScreen = new StartScreen();
-        //await this.startScreen.init(document.querySelector(".game-container"));
+        await this.startScreen.init(document.querySelector(".game-container"));
 
         this.map = new Map(window.Maps["TestingRoom"]);
         this.map.mountObjects();
