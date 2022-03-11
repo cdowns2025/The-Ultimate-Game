39f45d2838d4d72e37080d1f9b5b1c63fd4d6b5d
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-11 17:31:15 -0500

    Update UI.js

--- JS/UI.js
+++ JS/UI.js
@@ -10,6 +10,40 @@ class UI {
     this.score.innerHTML = (`
       <p>${this.actualScore}</p>
     `);
+    
+    if (this.actualScore === 6) {
+      this.health.innerHTML = (`
+        <img src="Heart.png" />
+        <img src="Heart.png" />
+        <img src="Heart.png" />
+      `);
+    } else if (this.actualScore === 5) {
+      this.health.innerHTML = (`
+        <img src="Heart.png" />
+        <img src="Heart.png" />
+        <img src="Half-Health.png" />
+      `); 
+    } else if (this.actualScore === 4) {
+      this.health.innerHTML = (`
+        <img src="Heart.png" />
+        <img src="Heart.png" />
+      `); 
+    } else if (this.actualScore === 3) {
+      this.health.innerHTML = (`
+        <img src="Heart.png" />
+        <img src="Half-Health.png" />
+      `); 
+    } else if (this.actualScore === 2) {
+      this.health.innerHTML = (`
+        <img src="Heart.png" />
+      `); 
+    } else if (this.actualScore === 1) {
+      this.health.innerHTML = (`
+        <img src="Half-Health.png" />
+      `); 
+    } else if (this.actualScore === 0) {
+      this.health.innerHTML = (``); 
+    }
   }
   
   createElements() {
