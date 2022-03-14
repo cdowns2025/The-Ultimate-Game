bf46e256bb69b75dbeec050a8d22a6945027013a
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-14 18:03:57 -0400

    Update UI.js

--- JS/UI.js
+++ JS/UI.js
@@ -51,6 +51,7 @@ class UI {
   update(game) {
     this.actualScore = game.score;
     this.playerHealth = game.map.gameObjects.allies["player"].health;
+    this.waveNumber = game.wave;
     
     this.score.innerHTML = (`
       <p>${this.actualScore}</p>
