f022d6df3fb29efad149e627677b1b584884fc8d
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-14 16:51:15 -0400

    Update init.js

--- JS/init.js
+++ JS/init.js
@@ -1,6 +1,8 @@
 //Wait for the window to load
 window.addEventListener("load", () => {
  
+  alert("Please click in the game screen to continue");
+ 
   //Create the game!
   const game = new Game({
     //Pass in the game container from the DOM
