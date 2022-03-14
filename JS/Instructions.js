15746bd69b013ec77c57ef772154a19e022fab65
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-14 16:24:13 -0400

    Update Instructions.js

--- JS/Instructions.js
+++ JS/Instructions.js
@@ -9,16 +9,14 @@ class Instructions {
     
     this.element.innerHTML = (`
       <p id="movement">Movement: Up, Down, Left Right</p>
-      <p id="dash">Dash</p>
-      <p id="attack">Attack</p>
+      <p id="dash">Dash (Space)</p>
+      <p id="attack">Attack (Enter)</p>
       <img id="W-Key" src="W-Key.png" />
       <img id="A-Key" src="A-Key.png" />
       <img id="S-Key" src="S-Key.png" />
       <img id="D-Key" src="D-Key.png" />
       <img id="Space" src="Space.png" />
-      <p id="space-description">Space</p>
       <img id="Enter" src="Enter.png" />
-      <p id="enter-description">Enter</p>
     `);
   }
   
