9b624e97416218a91db6591170e3d5ddaf8ae187
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-14 15:48:55 -0400

    Create Instructions.js

---
+++ JS/Instructions.js
@@ -0,0 +1,30 @@
+class Instructions {
+  constructor() {
+    this.element = null;
+  }
+  
+  createElement() {
+    this.element = document.createElement("div");
+    this.element.classList.add("Instructions");
+    
+    this.element.innerHTML = (`
+      <h1>Keys</h1>
+      <img id="W-Key" src="W-Key" />
+      <img id="A-Key" src="A-Key" />
+      <img id="S-Key" src="S-Key" />
+      <img id="D-Key" src="D-Key" />
+      <img id="Up-Arrow" src="Up-Arrow" />
+      <img id="Right-Arrow" src="Right-Arrow" />
+      <img id="Down-Arrow" src="Down-Arrow" />
+      <img id="Left-Arrow" src="Left-Arrow" />
+      <img id="Space" src="Space" />
+      <img id="Enter" src="Enter" />
+    `);
+  }
+  
+  init(container) {
+    this.createElement();
+    
+    container.appendChild(this.element);
+  }
+}
