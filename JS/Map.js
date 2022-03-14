1798f27ec22c1e94164acc82db0135923dc531db
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-14 19:56:39 -0400

    Update Map.js

--- JS/Map.js
+++ JS/Map.js
@@ -36,7 +36,7 @@ class Map {
                     x: utils.asGrid(Math.floor(Math.random() * 20)),
                     y: utils.asGrid(Math.floor(Math.random() * 15)),
                     enemyCapacity: this.waves[waveNumber].spawners[i + 1].basic,
-                    intervalTime: 1000,
+                    intervalTime: 2000,
                     color: "brown",
                 })
             }
