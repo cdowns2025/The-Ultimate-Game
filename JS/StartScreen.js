2d0a8cded8c89539742f73b04808e431c8ecc3cc
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-14 20:14:45 -0400

    Update StartScreen.js

--- JS/StartScreen.js
+++ JS/StartScreen.js
@@ -11,7 +11,23 @@ class StartScreen {
             <img src="logo.png" />
             <button>Start</button>
             <h1>Ship Defenders</h1>
+            <img id="credits" src="Credits.png" />
         `);
+        
+        this.element.getElementById("credits").addEventListener("click", () => {
+            this.element.remove();
+            this.element = document.createElement("div");
+            this.element.classList.add("Credits");
+            
+            this.element.innerHTML = (`
+                <h1>The team</h1>
+                <p>Carver Snyder: Music Creator and Logistics Manager</p>
+                <p>Tyler Wiggins: Game Art Creator</p>
+                <p>Carter Downs: Programmer, Music Creator, and Icon Creator</p>
+                <p>Have Gee: Programmer</p>
+                <p>Jimmy Giannasi: Concept Designer and Recorder</p>
+            `);
+        });
         
         this.element.querySelectorAll("button").forEach(button => {
             button.addEventListener("click", () => {
