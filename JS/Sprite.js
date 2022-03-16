aa2211d898900ea3fe3c0919eeb347efb8379c25
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-15 20:10:49 -0400

    Update Sprite.js

--- JS/Sprite.js
+++ JS/Sprite.js
@@ -1,24 +1,25 @@
 class Sprite {
     constructor(config) {
+        this.images = config.images;
+        
         this.image = new Image();
-        this.image.src = config.src;
+        this.image.src = this.images[0];
         this.image.onload = () => {
             this.isLoaded = true;
         }
         
         this.gameObject = config.gameObject;
+        
+        this.imageFrame = "forward";
     }
     
     draw(ctx, player) {
-        const x = this.gameObject.x + utils.asGrid(7.5) - player.x + 6;
-        const y = this.gameObject.y + utils.asGrid(4.5) - player.y + 6;
+        const x = this.gameObject.x + utils.asGrid(7.5) - player.x + 2;
+        const y = this.gameObject.y + utils.asGrid(4.5) - player.y - 1;
         
         if (this.gameObject.isRendered == true) {
-            //ctx.fillStyle = this.gameObject.color;
-            //ctx.fillRect(x, y, 5, 5);
-            let image = new Image();
-            image.src = "standF.PNG";
-            ctx.drawImage(image, x, y);
+            
+            ctx.drawImage(this.image, x, y);
         }
         //this.isLoaded && ctx.drawImage(this.image, x, y);
     }
