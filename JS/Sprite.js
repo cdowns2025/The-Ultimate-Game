af60322c3a9398d7be87be3633b92ee07282e7a0
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-15 20:55:55 -0400

    Update Sprite.js

--- JS/Sprite.js
+++ JS/Sprite.js
@@ -27,8 +27,8 @@ class Sprite {
         let y = this.gameObject.y + utils.asGrid(4.5) - player.y;
         
         if (this.images[this.imageFrame] === "floor_hole.png") {
-            x -= 2;
-            y += 2;
+            x -= 1;
+            y += 1;
         } else if (this.images[this.imageFrame] === "health.png") {
             y += 4;
             x += 4;
