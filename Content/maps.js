c9d57dc25358842cb3c882a7406e058872585cc7
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-15 20:34:44 -0400

    Update maps.js

--- Content/maps.js
+++ Content/maps.js
@@ -57,6 +57,7 @@ window.Maps = {
         ],
         waves: {
             "1": {
+                basic: 2,
                 /*spawners: {
                     "1": {
                         basic: 2,
