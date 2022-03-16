a613066d913bb0729d7f86213db9e5334abe3b24
cdowns2025 <76601488+cdowns2025@users.noreply.github.com>
2022-03-16 06:24:40 -0400

    Update Person.js

--- JS/Person.js
+++ JS/Person.js
@@ -114,10 +114,11 @@ class Person extends GameObject {
             this.hit = true;
             this.hitInterval = 30;
             this.health -= damageLevel;
+            
             if (this.direction = "down") this.direction = "up";
             else if (this.direction = "up") this.direction = "down";
             else if (this.direction = "left") this.direction = "right";
-            else this.direction = "up";
+            else this.direction = "left";
             this.movingProgressRemaining = 16;
         }
     }
